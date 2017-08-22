f3fd85ed7ee3f43c34f357397a5999d093d6352c
gizmo <rohrmoser.christoph91@gmail.com>
2017-08-22 16:46:46 +0200

    deleted all image data

--- config/db.php
+++ config/db.php
@@ -1,18 +1,18 @@
 <?php
 // dev environment db
-return [
-    'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=mh',
-    'username' => 'root',
-    'password' => 'angel1angel1',
-    'charset' => 'utf8',
-];
-
-// test environment db
 //return [
 //    'class' => 'yii\db\Connection',
-//    'dsn' => 'mysql:host=localhost;dbname=u102386db2',
-//    'username' => 'u102386db2',
+//    'dsn' => 'mysql:host=localhost;dbname=mh',
+//    'username' => 'root',
 //    'password' => 'angel1angel1',
 //    'charset' => 'utf8',
 //];
+
+//test environment db
+return [
+    'class' => 'yii\db\Connection',
+    'dsn' => 'mysql:host=localhost;dbname=u102386db2',
+    'username' => 'u102386db2',
+    'password' => 'angel1angel1',
+    'charset' => 'utf8',
+];
