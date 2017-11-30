84a618b22319dc013c79472ce59ad1b93467135f
gizmo <rohrmoser.christoph91@gmail.com>
2017-11-30 15:11:12 +0100

    newsletter mail improvements

--- views/backend/backendNewsletter.php
+++ views/backend/backendNewsletter.php
@@ -14,38 +14,39 @@ use yii\helpers\Url;
 use yii\widgets\Pjax;
 
 ?>
-
-<div class="row" id="row_newsletter">
-    <div class="col-xs-12">
-        <h1> Anmeldungen für den Newsletter </h1><?php
-      $dataProvider = new ActiveDataProvider([
-          'query' => Email::find(),
-          'pagination' => [
-              'pageSize' => 500,
-          ],
-      ]);
-      Pjax::begin();
-      echo GridView::widget([
-          'dataProvider' => $dataProvider,
-          'columns' => [
-              [
-                  'attribute' => 'email',
-                  'format' => 'text'
+<div class="container">
+    <div class="row" id="row_newsletter">
+        <div class="col-xs-12">
+            <h1> Anmeldungen für den Newsletter </h1><?php
+          $dataProvider = new ActiveDataProvider([
+              'query' => Email::find(),
+              'pagination' => [
+                  'pageSize' => 500,
               ],
-              [
-                  'header' => 'Aktionen',
-                  'class' => 'yii\grid\ActionColumn',
-                  'template' => '{delete}',
-                  'buttons' => [
-                      'delete' => function ($url, $model) {
-                          $url = Url::to(['backend/delete-newsletter-email', 'id' => $model->id]);
-                          return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, ['title' => 'delete']);
-                      },
-                  ]
+          ]);
+          Pjax::begin();
+          echo GridView::widget([
+              'dataProvider' => $dataProvider,
+              'columns' => [
+                  [
+                      'attribute' => 'email',
+                      'format' => 'text'
+                  ],
+                  [
+                      'header' => 'Aktionen',
+                      'class' => 'yii\grid\ActionColumn',
+                      'template' => '{delete}',
+                      'buttons' => [
+                          'delete' => function ($url, $model) {
+                              $url = Url::to(['backend/delete-newsletter-email', 'id' => $model->id]);
+                              return Html::a('<span class="glyphicon glyphicon-trash"></span>', $url, ['title' => 'delete']);
+                          },
+                      ]
+                  ],
               ],
-          ],
-      ]);
-      Pjax::end();
-      ?>
+          ]);
+          Pjax::end();
+          ?>
+        </div>
     </div>
 </div>
