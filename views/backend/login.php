55c323108ed9fed780dcdd8412040762303720c3
gizmo <rohrmoser.christoph91@gmail.com>
2017-12-12 12:49:58 +0100

    added partner site, resized card images, added english maps version, some minor fixes

--- views/backend/login.php
+++ views/backend/login.php
@@ -12,32 +12,34 @@ $this->title = 'Login';
 //$url = Url::to('login/index');
 $url = Url::to('/admin');
 ?>
-<div class="container">
-    <div class="row">
-        <div id="loginContainer">
-            <h1><?= Html::encode($this->title) ?></h1>
-            <p><?= Html::encode($model->getMessage()) ?></p>
-            <?php $form = ActiveForm::begin([
-                'action' => $url,
-                'options' => ['method' => 'post'],
-                'id' => 'login-form',
-                'layout' => 'horizontal',
-                'fieldConfig' => [
-                    'template' => "{label}\n<div class=\"col-lg-3\">{input}</div>\n<div class=\"col-lg-8\">{error}</div>",
-                    'labelOptions' => ['class' => 'col-lg-1 control-label'],
-                ],
-            ]); ?>
+<div id="backgroundLogin">
+    <div class="container">
+        <div class="row">
+            <div class="col-xs-1 col-sm-3 col-md-3 col-lg-4"> </div>
+            <div class="col-xs-10 col-sm-6 col-md-6 col-lg-4">
+                <div id="loginContainer">
+                    <h1><?= Html::encode($this->title) ?></h1>
+                    <p><?= Html::encode($model->getMessage()) ?></p>
+                    <?php $form = ActiveForm::begin([
+                        'action' => $url,
+                        'options' => ['method' => 'post'],
+                        'id' => 'login-form',
+                        'layout' => 'default',
+                    ]); ?>
 
-            <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Username') ?>
+                    <?= $form->field($model, 'username')->textInput(['autofocus' => true])->label('Username') ?>
 
-            <?= $form->field($model, 'password')->passwordInput()->label('Passwort') ?>
-            <div class="form-group">
-                <div class="col-lg-offset-1 col-lg-11">
-                    <?= Html::submitButton('Anmelden', ['class' => 'btn btn-primary', 'name' => 'login-button']) ?>
+                    <?= $form->field($model, 'password')->passwordInput()->label('Passwort') ?>
+                    <div class="form-group">
+                        <?= Html::submitButton('Anmelden', ['class' => 'btn btn-primary', 'name' => 'login-button', 'id' => 'loginButton']) ?>
+                    </div>
+
+
+                    <?php ActiveForm::end(); ?>
                 </div>
             </div>
-
-
-            <?php ActiveForm::end(); ?>
+            <div class="col-xs-1 col-sm-3 col-md-3 col-lg-4"> </div>
+        </div>
     </div>
+</div>
 
