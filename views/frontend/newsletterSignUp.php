7517a98e63f504bf02c1862f196b77b68fd2849d
gizmo <rohrmoser.christoph91@gmail.com>
2018-01-16 15:04:14 +0100

    added partner site, resized card images, added english maps version, some minor fixes

--- views/frontend/newsletterSignUp.php
+++ views/frontend/newsletterSignUp.php
@@ -6,29 +6,19 @@
  * Time: 11:25
  */
 
-use app\models\Email;
-use app\models\Image\ImageFactory;
 use yii\helpers\Html;
-use yii\helpers\Url;
 use yii\widgets\ActiveForm;
 use yii\widgets\Pjax;
 
 $this->title = 'Winzerhof Mayer-Hörmann | Newsletter-Anmeldung';
 
-$emailMdl = new Email();
-
-$imageMdl = ImageFactory::create('titleImage', 'newsletter');
-$image = $imageMdl -> getRandomImage();
-if (empty($image)) {
-    $imagePath = Url::to('/' . Yii::$app->params[0]['fallbackImagePath']);
-} else {
-    $imagePath = Url::to('/' . $image[0]['path'] . $image[0]['name']);
-}
-
 ?>
 <div class="container-fluid">
-    <div class="row">
-        <div class="standardTitleImageContainer" style='background-image: url(<?= $imagePath ?>);'></div>
+    <div id="row">
+        <div id="headlinePartner" class="row headline">
+            <h1> Newsletter-Anmeldung </h1>
+        </div>
+        <div class="row headlineBorderBottom"></div>
     </div>
 </div>
 
@@ -36,8 +26,6 @@ if (empty($image)) {
     <div class="row">
         <div class="col-xs-0 col-sm-1 col-md-3"></div>
         <div id="contentNewsletter" class="col-xs-12 col-sm-10 col-md-6">
-
-                <h1> Newsletter Anmeldung </h1>
                 <?php
                 Pjax::begin();
                 if($model->getEmailSaved()) {
